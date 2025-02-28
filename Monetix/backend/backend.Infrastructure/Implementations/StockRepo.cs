0cdfc66a0655edbbeef56ed0cf8819eeaf5ab0d6
Polar1s <k.nadelyaev17@gmail.com>
2025-02-28 18:04:33 +0200

    Update StockRepo.cs

--- Monetix/backend/backend.Infrastructure/Implementations/StockRepo.cs
+++ Monetix/backend/backend.Infrastructure/Implementations/StockRepo.cs
@@ -63,7 +63,7 @@ namespace backend.Infrastructure.Implementations
 
             var skipNumber = (query.PageNumber - 1) * query.PageSize;
 
-            return await stocks.Skip(skipNumber).Take(query.PageNumber).ToListAsync();
+            return await stocks.Skip(skipNumber).Take(query.PageSize ).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(Guid id)
